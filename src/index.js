2b9b4f4018875b92669750fe0a207da002e2f650
Omri Litov <omrilitov@gmail.com>
2016-02-01 19:24:56 +0200

    change api

--- src/index.js
+++ src/index.js
@@ -2,19 +2,21 @@
 
 import createError from 'http-errors';
 
-export default function (req, res, next) {
-  const auth = req.get('authorization');
+export default () => {
+  return (req, res, next) => {
+    const auth = req.get('authorization');
 
-  if (!auth) {
-    return res.status(401).set('WWW-Authenticate', 'Negotiate');
-  }
+    if (!auth) {
+      return res.status(401).set('WWW-Authenticate', 'Negotiate').end();
+    }
 
-  if (auth.lastIndexOf('Negotiate') !== 0) {
-    return next(createError(400, `Malformed authentication token ${auth}`));
-  }
+    if (auth.lastIndexOf('Negotiate') !== 0) {
+      return next(createError(400, `Malformed authentication token ${auth}`));
+    }
 
-  req.auth = req.auth || {};
-  req.auth.token = auth.substring('Negotiate '.length);
+    req.auth = req.auth || {};
+    req.auth.token = auth.substring('Negotiate '.length);
 
-  next();
-}
+    next();
+  };
+};
